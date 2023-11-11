068e6eb161e1e2f92eae6f25050d54deb11e84f0
akshat123436 <akshat123436@gmail.com>
2023-11-11 09:49:49 +0530

    Logo separate component created

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
+import Wrapper from '../assets/wrappers/LandingPage'
+// import logo from "../assets/images/logo.svg"
+import { Logo } from '../components'
+import main from "../assets/images/main.svg"
+import { Link } from 'react-router-dom'
 
 function Landing() {
   return (
-    <div>Landing</div>
+    // <Wrapper>
+    //   <nav>
+    //     <img src={logo} alt="Hired" className='logo'/>
+    //   </nav>
+    //   <div className='container page'>
+    //     <div className="info">
+    //       <h1>Hired : The Job <span> Tracking App</span></h1>
+    //       <p>Manage your Job applications at one place and get Hired</p>
+    //       <Link to='/register' className='btn register-link'>Register</Link>
+    //       <Link to='/login' className='btn'>Login / Demo User</Link>
+    //     </div>
+    //     <img src={main} alt="" />
+    //   </div>
+
+    // </Wrapper>
+    <Wrapper>
+    <nav>
+      {/* <img src={logo} alt='Hired' className='logo' />/ */}
+      <Logo></Logo>
+    </nav>
+    <div className='container page'>
+      {/* info */}
+      <div className='info'>
+        <h1>
+        Hired : The Job <span> Tracking App</span>
+        </h1>
+        <p>
+        Manage your Job applications at one place and get Hired
+        </p>
+        <Link to='/register' className='btn register-link'>
+          Register
+        </Link>
+        <Link to='/login' className='btn'>
+          Login / Demo User
+        </Link>
+      </div>
+      <img src={main} alt='job hunt' className='img main-img' />
+    </div>
+  </Wrapper>
   )
 }
 
-export default Landing
+export default Landing;
