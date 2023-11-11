c9ff24086557196a6e041d68d27d7e96ec8534b4
akshat123436 <akshat123436@gmail.com>
2023-11-11 10:51:02 +0530

    Error page completed

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -7,24 +7,8 @@ import { Link } from 'react-router-dom'
 
 function Landing() {
   return (
-    // <Wrapper>
-    //   <nav>
-    //     <img src={logo} alt="Hired" className='logo'/>
-    //   </nav>
-    //   <div className='container page'>
-    //     <div className="info">
-    //       <h1>Hired : The Job <span> Tracking App</span></h1>
-    //       <p>Manage your Job applications at one place and get Hired</p>
-    //       <Link to='/register' className='btn register-link'>Register</Link>
-    //       <Link to='/login' className='btn'>Login / Demo User</Link>
-    //     </div>
-    //     <img src={main} alt="" />
-    //   </div>
-
-    // </Wrapper>
     <Wrapper>
     <nav>
-      {/* <img src={logo} alt='Hired' className='logo' />/ */}
       <Logo></Logo>
     </nav>
     <div className='container page'>
