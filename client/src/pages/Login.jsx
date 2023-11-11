714032c9fcc1a22401fd76ecad9a56d0696e1728
akshat123436 <akshat123436@gmail.com>
2023-11-11 13:34:23 +0530

    login register page done

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-Link
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage'
+import { Logo, FormRow } from '../components'
 function Login() {
   return (
-    <div>
-
-    <div>Login</div>
-    <Link to="/register">Register</Link>
-    </div>
+    <Wrapper>
+    <form className='form'>
+      <Logo />
+      <h4>Login</h4>
+      <FormRow type='email' name='email' defaultValue='akshat@gmail.com' />
+      <FormRow type='password' name='password' defaultValue='elegantSecret' />
+      <button type='submit' className='btn btn-block'>
+        submit
+      </button>
+      <button type='button' className='btn btn-block'>
+        explore the app
+      </button>
+      <p>
+        Not a member yet?
+        <Link to='/register' className='member-btn'>
+          Register
+        </Link>
+      </p>
+    </form>
+  </Wrapper>
   )
 }
 
