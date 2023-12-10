4a5b2bb6a569313473824c8d6c01f44ee9e11df7
akshat123436 <akshat123436@gmail.com>
2023-12-10 18:51:01 +0530

    toggle theme button

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { FaAlignLeft } from "react-icons/fa";
 import Wrapper from "../assets/wrappers/Navbar";
 import { useDashboardContext } from "../pages/DashboardLayout";
 import LogoutContainer from "./LogoutContainer";
+import ThemeToggle from "./ThemeToggle";
 import Logo from "./Logo";
 function Navbar() {
   const { toggleSidebar } = useDashboardContext();
@@ -17,6 +18,7 @@ function Navbar() {
           <h4 className="logo-text">dashboard</h4>
         </div>
         <div className="btn-container">
+          <ThemeToggle></ThemeToggle>
           <LogoutContainer></LogoutContainer>
         </div>
       </div>
