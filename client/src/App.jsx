35418c3abecf137fcf90b079a0435b40e7444c50
akshat123436 <akshat123436@gmail.com>
2023-12-11 07:16:05 +0530

    dark mode added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,64 +1,80 @@
-import React from 'react'
-import {createBrowserRouter, RouterProvider} from "react-router-dom"
-import { HomeLayout, Landing, Register, Login, DashboardLayout, Error, AddJob, Stats, AllJobs, Profile, Admin } from './pages'
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  HomeLayout,
+  Landing,
+  Register,
+  Login,
+  DashboardLayout,
+  Error,
+  AddJob,
+  Stats,
+  AllJobs,
+  Profile,
+  Admin,
+} from "./pages";
+
+const checkDefaultTheme = () => {
+  const isDarkTheme = localStorage.getItem("darkTheme") === "true";
+  document.body.classList.toggle("dark-theme", isDarkTheme);
+  return isDarkTheme;
+};
+
+const isDarkThemeEnabled = checkDefaultTheme();
+
 const router = createBrowserRouter([
   {
-    path:"/",
+    path: "/",
     element: <HomeLayout></HomeLayout>,
-    errorElement : <Error></Error>,
-    children:[
+    errorElement: <Error></Error>,
+    children: [
       {
-        index:true,
-        element:<Landing />
+        index: true,
+        element: <Landing />,
       },
       {
-        path:'/register',
-        element:(
-          <Register></Register>
-        )
+        path: "/register",
+        element: <Register></Register>,
       },
       {
-        path:'/dashboard',
-        element:(
-          <DashboardLayout></DashboardLayout>
+        path: "/dashboard",
+        element: (
+          <DashboardLayout
+            isDarkThemeEnabled={isDarkThemeEnabled}
+          ></DashboardLayout>
         ),
-        children:[
+        children: [
           {
-            index:true,
-            element:<AddJob/>
+            index: true,
+            element: <AddJob />,
           },
           {
-            path:'stats',
-            element:<Stats></Stats>
+            path: "stats",
+            element: <Stats></Stats>,
           },
           {
-            path:"all-jobs",
-            element:<AllJobs></AllJobs>
+            path: "all-jobs",
+            element: <AllJobs></AllJobs>,
           },
           {
-            path:"profile",
-            element:<Profile></Profile>
+            path: "profile",
+            element: <Profile></Profile>,
           },
           {
-            path:"admin",
-            element:<Admin></Admin>
-          }
-        ]
+            path: "admin",
+            element: <Admin></Admin>,
+          },
+        ],
       },
       {
-        path:'/login',
-        element:(
-          <Login></Login>
-        )
-      }
-    ]
+        path: "/login",
+        element: <Login></Login>,
+      },
+    ],
   },
-  
-])
+]);
 function App() {
-  return (
-    <RouterProvider router={router} />
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -1,45 +1,52 @@
-import React, { createContext, useContext, useState } from 'react'
-import { BigSidebar, SmallSidebar, Navbar } from '../components'
-import { Outlet } from 'react-router-dom'
-import Wrapper from '../assets/wrappers/Dashboard'
+import React, { createContext, useContext, useState } from "react";
+import { BigSidebar, SmallSidebar, Navbar } from "../components";
+import { Outlet } from "react-router-dom";
+import Wrapper from "../assets/wrappers/Dashboard";
 const DashboardContext = createContext();
-function DashboardLayout() {
-  const user = {name : "akshat"};
+function DashboardLayout({ isDarkThemeEnabled }) {
+  const user = { name: "akshat" };
   const [showSidebar, setShowSidebar] = useState(false);
-  const [isDarkTheme, setisDarkTheme] = useState(false);
+  const [isDarkTheme, setisDarkTheme] = useState(isDarkThemeEnabled);
 
-  const toggleDarkTheme = ()=>{
-    console.log("toggle");
-  }
-  const toggleSidebar = ()=>{
+  const toggleDarkTheme = () => {
+    const newDarkTheme = !isDarkTheme;
+    setisDarkTheme(newDarkTheme);
+    document.body.classList.toggle("dark-theme", newDarkTheme);
+    localStorage.setItem("darkTheme", newDarkTheme);
+  };
+  const toggleSidebar = () => {
     setShowSidebar(!showSidebar);
-  }
-  const logoutUser = ()=>{
+  };
+  const logoutUser = () => {
     console.log("logout");
-  }
+  };
 
   return (
     <DashboardContext.Provider
-      value = {
-        {
-          user, showSidebar, isDarkTheme, toggleDarkTheme, toggleSidebar, logoutUser
-        }
-      }
+      value={{
+        user,
+        showSidebar,
+        isDarkTheme,
+        toggleDarkTheme,
+        toggleSidebar,
+        logoutUser,
+      }}
     >
-    <Wrapper>
-    <main className="dashboard">
-      <SmallSidebar></SmallSidebar>
-      <BigSidebar></BigSidebar>
-      <div>
-        <Navbar>
-
-        <div className='dashboard-page'><Outlet/></div>
-        </Navbar>
-      </div>
-    </main>
-    </Wrapper>
+      <Wrapper>
+        <main className="dashboard">
+          <SmallSidebar></SmallSidebar>
+          <BigSidebar></BigSidebar>
+          <div>
+            <Navbar>
+              <div className="dashboard-page">
+                <Outlet />
+              </div>
+            </Navbar>
+          </div>
+        </main>
+      </Wrapper>
     </DashboardContext.Provider>
-  )
+  );
 }
-export const useDashboardContext = ()=> useContext(DashboardContext);
-export default DashboardLayout
+export const useDashboardContext = () => useContext(DashboardContext);
+export default DashboardLayout;
